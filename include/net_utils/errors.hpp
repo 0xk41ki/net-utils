f091f4e9612c074559507acc1385a1c30d55a7a8
Jalaj <96870071+j414j@users.noreply.github.com>
2026-06-14 16:30:21 +0530

    errors: added an initial error module

---
+++ include/net_utils/errors.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <expected>
+#include <string_view>
+
+namespace net_utils {
+enum class NetErrorCode {
+  AddressResolutionFailed,
+  AllocFailure,
+};
+
+constexpr std::string_view to_string(NetErrorCode code) {
+  switch (code) {
+  case NetErrorCode::AddressResolutionFailed:
+    return "AddressResolutionFailed";
+  case NetErrorCode::AllocFailure:
+    return "AllocFailure";
+  }
+
+  return "UnknownError";
+}
+
+template <typename T> class NetError {
+public:
+  explicit NetError(NetErrorCode code) : code_(code) {};
+  NetError(NetErrorCode code, T data) : code_(code), data_(data) {};
+  NetErrorCode code() const noexcept { return code_; };
+  const T &data() const noexcept { return data_; };
+
+private:
+  NetErrorCode code_;
+  T data_;
+};
+
+template <> class NetError<void> {
+public:
+  explicit NetError(NetErrorCode code) : code_(code) {};
+  NetErrorCode code() const noexcept { return code_; };
+
+private:
+  NetErrorCode code_;
+};
+
+template <typename T, typename E = void>
+using NetResult = std::expected<T, NetError<E>>;
+}; // namespace net_utils
